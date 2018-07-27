6ba36f763a83ffad51ee0a8fbd71c0e4abaca614
swnb <1786718956@qq.com>
2018-07-27 22:32:09 +0800

    fix problem

--- src/tpscript/util/getdirection.ts
+++ src/tpscript/util/getdirection.ts
@@ -1,24 +1,26 @@
 type Pos = [number, number];
 
+const getCosDeg = (linA: Pos, linB: Pos): number => {
+    const axb = linA[0] * linB[0] + linA[1] * linB[1];
+
+    const ab =
+        Math.sqrt(Math.pow(linA[0], 2) + Math.pow(linA[1], 2)) *
+        Math.sqrt(Math.pow(linB[0], 2) + Math.pow(linB[1], 2));
+    return parseFloat(axb.toFixed(3)) / parseFloat(ab.toFixed(3));
+};
+
 export const getDirection = (
     startPos: Pos,
     endPos: Pos,
     points: Pos[]
 ): Pos => {
-    const [ey, ex] = [endPos[0], endPos[1]];
-    const [sy, sx] = [startPos[0], startPos[1]];
+    const [ex, ey] = [endPos[0], endPos[1]];
+    const [sx, sy] = [startPos[0], startPos[1]];
 
     const k = parseFloat(((ey - sy) / (ex - sx)).toFixed(3));
-
-    // console.log(ey - sy, ex - sx);
-    // console.log(k);
-
     const proportion = parseFloat((-1 / k).toFixed(3));
 
-    // let direction: [Pos, Pos] = [[1, proportion], [-1, -proportion]];
     let direction: Pos = [1, proportion];
-
-    const long = 20;
 
     const somePoints = points.find(
         (ele: Pos): boolean =>
@@ -28,15 +30,25 @@ export const getDirection = (
                 : true
     ) as Pos;
 
-    // x1*y2-x2*y1
+    const middlePoints = [(ex + sx) / 2, (ey + sy) / 2];
+
+    const someLine: Pos = [
+        somePoints[0] - middlePoints[0],
+        somePoints[1] - middlePoints[1]
+    ];
+
+    console.log("some line ", someLine);
+
+    console.log("direction ", direction);
 
     let ele: Pos;
 
-    console.log(somePoints, direction);
+    console.log(someLine[0] * direction[1] - direction[0] * someLine[1]);
 
-    console.log(somePoints[0] * direction[1] - direction[0] * somePoints[1]);
+    // 线段的长度
+    const long = 20;
 
-    if (somePoints[0] * direction[1] - direction[0] * somePoints[1] > 0) {
+    if (getCosDeg(someLine, direction) > 0) {
         console.log("right");
         ele = direction;
     } else {
@@ -44,43 +56,11 @@ export const getDirection = (
         console.log("reverse");
     }
 
-    // console.log(proportion);
     const x = parseFloat(
         Math.sqrt(Math.pow(long, 2) / (Math.pow(proportion, 2) + 1)).toFixed(3)
     );
 
-    // console.log(x);
-
     const y = proportion * x;
 
-    // console.log(y);
-
-    // console.log(x * ele[0], y * ele[0]);
-
     return [x * ele[0], y * ele[0]];
-
-    //修正
-    // direction = direction.map(
-    //     (ele: Pos): Pos => {
-    //         const proportion = parseFloat((ele[1] / ele[0]).toFixed(3));
-    //         // console.log(proportion);
-    //         const x = parseFloat(
-    //             Math.sqrt(
-    //                 Math.pow(long, 2) / (Math.pow(proportion, 2) + 1)
-    //             ).toFixed(3)
-    //         );
-
-    //         // console.log(x);
-
-    //         const y = proportion * x;
-
-    //         // console.log(y);
-
-    //         // console.log(x * ele[0], y * ele[0]);
-
-    //         return [x * ele[0], y * ele[0]];
-    //     }
-    // ) as [Pos, Pos];
-
-    // return direction;
 };
