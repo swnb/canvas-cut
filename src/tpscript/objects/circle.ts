e471c15b0e38f3d81cec3ff82aa91d6934fe7ffa
swnb <1786718956@qq.com>
2018-07-28 15:47:20 +0800

    save change rm circle

--- src/tpscript/objects/circle.ts
+++ src/tpscript/objects/circle.ts
@@ -13,6 +13,9 @@ export class Circle extends ControObj {
         typecode: 1
     };
 
+    // 点阵信息
+    public polygonPoints: Array<Pos> = [];
+
     public middlePoint: [number, number] = [0, 0];
     public r: number = 0;
 
@@ -31,10 +34,36 @@ export class Circle extends ControObj {
     init(objType: ObjType) {
         this.objType = objType;
         this.middlePoint = [this.x + this.width / 2, this.y + this.height / 2];
+        this.x -= 50;
+        this.y -= 50;
+
+        this.polygonPoints = [[this.x, this.y], this.middlePoint];
+        return this;
+    }
+
+    update(x: number, y: number) {
+        const [xdivi, ydivi] = [x - this.x, y - this.y];
+
+        // 更新中心点
+        this.middlePoint = [
+            this.middlePoint[0] + xdivi,
+            this.middlePoint[1] + ydivi
+        ];
+
+        // 更新数据点阵的信息
+        this.polygonPoints = this.polygonPoints.map(
+            (point: Pos): Pos => {
+                return [point[0] + xdivi, point[1] + ydivi];
+            }
+        );
+
         return this;
     }
 
     draw() {
+        // 画出图标
+        this.drawIcon();
+
         switch (this.objType.typecode) {
             case 1: {
                 this.circle(this.middlePoint[0], this.middlePoint[1], this.r);
