5c3a9c53e8618697f379cd29de78690ae5c589df
swnb <1786718956@qq.com>
2018-08-08 13:04:20 +0800

    大致整体上完成了

--- src/tpscript/objects/neo.ts
+++ src/tpscript/objects/neo.ts
@@ -66,6 +66,9 @@ export class Neo extends ControObj {
         this.direct = this.getDirect();
 
         this.sortLine();
+
+        // 得到位置点阵的信息,更新物体
+        [this.width, this.height, this.x, this.y] = this.getWidthHeight();
     }
 
     getDirect(): Pos {
@@ -75,6 +78,41 @@ export class Neo extends ControObj {
             [...closeLine.points[1]] as [number, number],
             [[...this.lines[0].points[1]] as [number, number]]
         );
+    }
+
+    getWidthHeight(): [number, number, number, number] {
+        // 分析点阵数据得到实际的最大值和最小值
+
+        const firstPoint = this.lines[0].points;
+
+        let [minX, maxX, minY, maxY] = [
+            firstPoint[0][0],
+            firstPoint[0][0],
+            firstPoint[0][1],
+            firstPoint[0][1]
+        ];
+
+        this.lines.forEach((line: Straight | Curve) => {
+            const point = line.points[0];
+            if (minX > point[0]) {
+                minX = point[0];
+            }
+            if (maxX < point[0]) {
+                maxX = point[0];
+            }
+            if (minY > point[1]) {
+                minY = point[1];
+            }
+            if (maxY < point[1]) {
+                maxY = point[1];
+            }
+        });
+
+        const width = maxX - minX;
+
+        const height = maxY - minY;
+
+        return [width, height, minX, minY];
     }
 
     init() {
@@ -191,10 +229,4 @@ export class Neo extends ControObj {
 
         return this;
     }
-
-    line2Point() {
-        
-    }
-
-    // set polygonPoints() {}
 }
