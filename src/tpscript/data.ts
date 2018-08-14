c982673d0bf2581d428122120ebc1caaa3bdae2f
张冠宇 <zhangguanyu@zhangguanyudeMacBook-Pro.local>
2018-08-14 23:05:17 +0800

    loading paga before img load

---
+++ src/tpscript/data.ts
@@ -0,0 +1,6 @@
+interface dataStruct {
+	[propname: string]: string;
+}
+export const imgs: dataStruct = Object.create(null);
+
+export const solveImgs = () => {};
---
+++ src/tpscript/loading.ts
@@ -0,0 +1,77 @@
+import util from "./util/util";
+
+type Pos = [number, number];
+
+export class LoadingPage {
+	private context: CanvasRenderingContext2D;
+
+	private width: number;
+	private height: number;
+
+	private top: Pos = [this.width / 2, this.height / 4];
+	private right: Pos = [(this.width * 3) / 4, this.height / 2];
+	private bottom: Pos = [this.width / 2, (this.height * 3) / 4];
+	private left: Pos = [this.width / 4, this.height / 2];
+	private polygenPoint: Pos[] = [];
+
+	private startPos: Pos = [this.width / 2, this.height / 2 - 10]; // 起点
+	private midPos: Pos = [this.width / 2, this.height / 2]; // 圆心点
+	private movePos: Pos = [this.width / 2, this.height / 2 - 10];
+
+	constructor(
+		context: CanvasRenderingContext2D,
+		width: number,
+		height: number
+	) {
+		this.context = context;
+		this.width = width;
+		this.height = height;
+	}
+
+	tick() {}
+
+	init() {
+		//  *
+		// * *
+		//  *
+
+		this.startPos = [];
+
+		this.draw();
+	}
+
+	rotate() {
+		util.affineTransform(this.startPos, this.midPos);
+		this.polygenPoint = this.polygenPoint.map(
+			(pos: Pos): Pos => {
+				return [0, 0];
+			}
+		);
+	}
+
+	stretch() {
+		this.polygenPoint.map(
+			(pos: Pos): Pos => {
+				return [0, 0];
+			}
+		);
+	}
+
+	update() {}
+	draw() {
+		const [preFillStyle, preStrokeStyle] = [
+			this.context.fillStyle,
+			this.context.strokeStyle
+		];
+		this.context.beginPath();
+		this.context.moveTo(this.top[0], this.top[1]);
+		this.context.lineTo(this.right[0], this.right[1]);
+		this.context.lineTo(this.bottom[0], this.bottom[1]);
+		this.context.lineTo(this.left[0], this.left[1]);
+		this.context.fill();
+		this.context.stroke();
+		this.context.closePath();
+		this.context.fillStyle = preFillStyle;
+		this.context.strokeStyle = preStrokeStyle;
+	}
+}
