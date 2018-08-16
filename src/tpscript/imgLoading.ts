9e601a9634e0e6690259e926f6b15716aa701a42
swnb <1786718956@qq.com>
2018-08-16 14:54:48 +0800

    让图片加载的模块去通讯

--- src/tpscript/imgLoading.ts
+++ src/tpscript/imgLoading.ts
@@ -1,4 +1,5 @@
-import { imgs } from "./data";
+import { imgSrcMap } from "./data";
+import { Center } from "./communication/commu";
 
 interface ImgMap {
 	[propname: string]: HTMLImageElement;
@@ -6,31 +7,28 @@ interface ImgMap {
 
 const _imgMap: ImgMap = Object.create(null);
 
-// 这个函数只执行一次
-export const load = (cb: () => void): (() => ImgMap | boolean) => {
-	let lock = true; // 上锁
+const emit = Center.setNewEvent("imgOnload");
 
-	let imgLength = Object.keys(imgs).length;
+// 这个函数只执行一次
+export const load = (): void => {
+	let imgLength = Object.keys(imgSrcMap).length;
 
 	const imgMap: ImgMap = new Proxy(_imgMap, {
 		get: (target: ImgMap, props: string) => target[props],
 		set: (target: ImgMap, prop: string, value: HTMLImageElement) => {
 			target[prop] = value;
 			if (--imgLength === 0) {
-				lock = false; // 解锁
-				cb();
+				emit(imgMap);
 			}
 			return true;
 		}
 	});
 
-	for (const tag in imgs) {
+	for (const src in imgSrcMap) {
 		const Img = new Image();
-		Img.src = imgs[tag];
+		Img.src = imgSrcMap[src];
 		Img.onload = () => {
-			imgMap[tag] = Img;
+			imgMap[src] = Img;
 		};
 	}
-
-	return () => (lock ? false : imgMap);
 };
